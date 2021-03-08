5619245293d65effee59d6371f865298a3b36364
Sneaky <78713939+SneakyHub@users.noreply.github.com>
2021-03-08 18:28:01 +0000

    Base script
    extremely basic. I run this every 4 hours on my machine to make sure everything is updated. I may expand on this in the future.

---
+++ auto-updater.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "==================================================="
+echo ""
+echo "Thank you for using Sneaky's Auto Updater! <3"
+echo ""
+echo "              Made with Love <3"
+echo "            Supports Ubuntu 18.04"
+echo "              Auto Updating Core"
+echo ""
+echo "==================================================="
+
+
+apt update -y && apt upgrade -y
+
+echo "Completed!"
